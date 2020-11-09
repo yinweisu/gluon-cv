c2a401e5fcf7a7edca3be5811c1c03e52badb117
yinweisu <wyin@ucdavis.edu>
2020-11-09 12:49:23 -0800

    rebase

--- tests/unittests/mini_coco.py
+++
@@ -1,7 +0,0 @@
-from gluoncv import data
-
-class COCODetectionMini(data.COCODetection):
-    CLASSES = ['bicycle', 'motorcycle']
-
-class COCOInstanceMini(data.COCOInstance):
-    CLASSES = ['bicycle', 'motorcycle']
