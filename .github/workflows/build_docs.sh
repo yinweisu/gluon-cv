c91061ac5c0b262d77d9685641435906f7741039
yinweisu <wyin@ucdavis.edu>
2020-11-16 11:23:49 -0800

    test build docs

---
+++ .github/workflows/build_docs.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+BRANCH=$(basename $1)
+
+mkdir -p ~/.mxnet/datasets
+for f in /mnt/efs/fs1/.mxnet/datasets/*; do
+    if [ -d "$f" ]; then
+        # Will not run if no directories are available
+        ln -s $f ~/.mxnet/datasets/$(basename "$f")
+    fi
+done
+
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
+cd docs 
+make html
+sed -i.bak 's/33\\,150\\,243/23\\,141\\,201/g' build/html/_static/material-design-lite-1.3.0/material.blue-deep_orange.min.css;
+sed -i.bak 's/2196f3/178dc9/g' build/html/_static/sphinx_materialdesign_theme.css;
+sed -i.bak 's/pre{padding:1rem;margin:1.5rem\\s0;overflow:auto;overflow-y:hidden}/pre{padding:1rem;margin:1.5rem 0;overflow:auto;overflow-y:scroll}/g' build/html/_static/sphinx_materialdesign_theme.css
+
+COMMAND_EXIT_CODE=$?
+if [[ $BRANCH == master ]]; then
+	# aws s3 cp s3://gluon-cv.mxnet.io/coverage.svg build/html/coverage.svg
+	# aws s3 sync --delete build/html/ s3://gluon-cv.mxnet.io/ --acl public-read --cache-control max-age=7200
+	# aws s3 cp build/html/coverage.svg s3://gluon-cv.mxnet.io/coverage.svg --acl public-read --cache-control max-age=300
+	# echo "Uploaded doc to http://gluon-cv.mxnet.io"
+	echo master
+elif [[ -d $SAVED_OUTPUT ]]; then
+	# aws s3 cp s3://gluon-vision-staging/${env.BRANCH_NAME}/${env.BUILD_NUMBER}/coverage.svg build/html/coverage.svg
+	# aws s3 sync --delete build/html/ s3://gluon-vision-staging/${env.BRANCH_NAME}/${env.BUILD_NUMBER}/ --acl public-read
+	# echo "Uploaded doc to http://gluon-vision-staging.s3-website-us-west-2.amazonaws.com/${env.BRANCH_NAME}/${env.BUILD_NUMBER}/index.html"
+	echo $BRANCH
+fi;
+exit $COMMAND_EXIT_CODE
